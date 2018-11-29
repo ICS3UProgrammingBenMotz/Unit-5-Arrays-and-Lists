640c3b6e4a4f82e22b802d28bc67e6bdd67428f4
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-29 11:01:59 -0500

    max val

---
+++ FindMaxVal(BenM)/FindMaxVal(BenM)/FindMaxValForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FindMaxVal_BenM_
+{
+    partial class frmFindMaxVal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbs = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumbs
+            // 
+            this.lstNumbs.FormattingEnabled = true;
+            this.lstNumbs.Location = new System.Drawing.Point(113, 58);
+            this.lstNumbs.Name = "lstNumbs";
+            this.lstNumbs.Size = new System.Drawing.Size(162, 199);
+            this.lstNumbs.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(142, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 40);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmFindMaxVal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 316);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstNumbs);
+            this.Name = "frmFindMaxVal";
+            this.Text = "Find Max Value";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbs;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ FindMaxVal(BenM)/FindMaxVal(BenM)/FindMaxValForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Ben M
+ * Created on: 29-11-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Find Max Value
+ * This program finds the max value
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMaxVal_BenM_
+{
+    public partial class frmFindMaxVal : Form
+    {
+        private int GetMaxValue(int[] tmpArrayOfInts)
+        {
+            tmpMaxVal;
+
+            return tmpMaxVal;
+        }
+
+        public frmFindMaxVal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //delcares variables
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMB = 500;
+            int[] arrayOfInts = new int[MAX_ARRAY_SIZE];
+            int counter, randNumb, maxVal;
+
+            //gets max value in array
+            maxVal = GetMaxValue(arrayOfInts);
+        }
+    }
+}
