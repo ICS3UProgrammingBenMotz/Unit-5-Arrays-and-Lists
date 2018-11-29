34b335c4ecd470648dbd978c3cf64ce6f2888ab9
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-29 10:37:52 -0500

    update

---
+++ ArrayAverage(BenM)/ArrayAverage(BenM)/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ArrayAverage_BenM_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbs = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCalculateAv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumbs
+            // 
+            this.lstNumbs.FormattingEnabled = true;
+            this.lstNumbs.Location = new System.Drawing.Point(178, 73);
+            this.lstNumbs.Name = "lstNumbs";
+            this.lstNumbs.Size = new System.Drawing.Size(144, 199);
+            this.lstNumbs.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(204, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCalculateAv
+            // 
+            this.btnCalculateAv.Location = new System.Drawing.Point(191, 293);
+            this.btnCalculateAv.Name = "btnCalculateAv";
+            this.btnCalculateAv.Size = new System.Drawing.Size(118, 23);
+            this.btnCalculateAv.TabIndex = 2;
+            this.btnCalculateAv.Text = "Calculate Average";
+            this.btnCalculateAv.UseVisualStyleBackColor = true;
+            this.btnCalculateAv.Click += new System.EventHandler(this.btnCalculateAv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 328);
+            this.Controls.Add(this.btnCalculateAv);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstNumbs);
+            this.Name = "Form1";
+            this.Text = "Array Average";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbs;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCalculateAv;
+    }
+}
+
---
+++ ArrayAverage(BenM)/ArrayAverage(BenM)/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by: Ben M
+ * Created on: 29-11-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Introduction to Arrays
+ * This program calculates the average of random numbers
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayAverage_BenM_
+{
+    public partial class Form1 : Form
+    {
+        //Creates conts and array
+        const int MAX_ARRAY_SIZE=10;
+        int[] arrayOfNumbers = new int[MAX_ARRAY_SIZE];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //creates variables
+            int randNumber;
+            int counter;
+            Random randNumbGen = new Random();
+
+            //Clears list box
+            this.lstNumbs.Items.Clear();
+
+            for (counter=0; counter < MAX_ARRAY_SIZE;counter++)
+            {
+                //generates new number
+                randNumber = randNumbGen.Next(1, MAX_ARRAY_SIZE + 1);
+
+                //assigns randNumb to counterin the array
+                arrayOfNumbers[counter] = randNumber;
+
+                //adds numb to list box
+                this.lstNumbs.Items.Add(randNumber);
+          
+                //refreshes form
+                this.Refresh();
+            }
+        }
+
+        private void btnCalculateAv_Click(object sender, EventArgs e)
+        {
+            //declares variables
+            double average = 0;
+            double sum = 0;
+            int counter;
+
+            //calculates sum
+            for (counter = 0; counter < arrayOfNumbers.Length; counter++)
+            {
+                sum = sum + arrayOfNumbers[counter];
+            }
+
+            //calculates sum
+            average = sum / arrayOfNumbers.Length;
+
+            //displdays average in message box
+            MessageBox.Show("The average is: " + average);
+        }
+    }
+}
