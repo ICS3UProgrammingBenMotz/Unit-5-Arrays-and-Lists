cc512a7dd052851e47588e763935f48c99fa45eb
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-12-05 09:55:08 -0500

    asdae

---
+++ 2DArrays(BenM)/2DArrays(BenM)/2DArrayForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _2DArrays_BenM_
+{
+    partial class frm2DArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudHeight = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudHeight
+            // 
+            this.nudHeight.Location = new System.Drawing.Point(117, 43);
+            this.nudHeight.Name = "nudHeight";
+            this.nudHeight.Size = new System.Drawing.Size(120, 20);
+            this.nudHeight.TabIndex = 0;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(117, 95);
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(120, 20);
+            this.nudWidth.TabIndex = 1;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(63, 45);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Width";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(263, 60);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(93, 34);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(25, 136);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(331, 173);
+            this.lstNumbers.TabIndex = 5;
+            this.lstNumbers.SelectedIndexChanged += new System.EventHandler(this.lstNumbers_SelectedIndexChanged);
+            // 
+            // frm2DArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 322);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudHeight);
+            this.Name = "frm2DArray";
+            this.Text = "2D Array";
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudHeight;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstNumbers;
+    }
+}
+
---
+++ 2DArrays(BenM)/2DArrays(BenM)/2DArrayForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DArrays_BenM_
+{
+    public partial class frm2DArray : Form
+    {
+
+        Random randNumbGen;
+
+        public frm2DArray()
+        {
+            InitializeComponent();
+            randNumbGen = new Random();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //clears listbox
+            lstNumbers.Items.Clear();
+
+            //declares variables
+            int width;
+            int length;
+            string pieText = "";
+            int randNumb;
+
+            //assigns length and width to text boxes
+            width = Convert.ToInt16(nudWidth.Value);
+            length = Convert.ToInt16(nudHeight.Value);
+
+            //creates array
+            int[,]a2DArray = new int[width, length];
+
+            //for loop for width
+            for (int widthCount = 0; widthCount < width; widthCount++)
+            {
+                //for loop for length
+                for (int lengthCount = 0; lengthCount < length; lengthCount++)
+                {
+                    //generates rand number
+                    randNumb = randNumbGen.Next(0, 9 + 1);
+
+                    //inserts rand number in array
+                    a2DArray[widthCount, lengthCount] = randNumb;
+
+                    // join the random number to the piece of text that will then go into the listbox
+                    pieText = pieText + " " + randNumb;
+
+                }
+                pieText = pieText + "\r" + "\n";
+            }
+            this.lstNumbers.Items.Add(pieText);
+        }
+
+        private void lstNumbers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
